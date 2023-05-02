35e86647328babbb89a0501fc2d34b16ce2d37d2
PC 2 <PC 2@DESKTOP-8ELQAGK>
2023-05-02 09:25:55 +0430

    Add project files.

---
+++ LINQ.WebApplication2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LINQ.WebApplication2.Global" Language="C#" %>
---
+++ LINQ.WebApplication2/page/LINQ.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LINQ.WebApplication2.page
+{
+    public partial class LINQ : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var allKeys = new string[] { "AA", "BB", "CC", "p_AA", "p_BB", "p_AA" };
+            var pKeys = allKeys.Where(k => k.StartsWith("p_"));
+            var @params = pKeys
+                .Distinct()
+                .Select(k => new KeyValuePair<string, string>(k.Substring(2), k));
+            var param = @params.LastOrDefault();
+
+        }
+    }
+}
